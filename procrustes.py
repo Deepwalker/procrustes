b908f282a96de77ec42a251f56ff896ed7ce444f
Alexander Solovyov <alexander@solovyov.net>
2011-01-30 22:03:24 +0100

    no love for leading underscores

--- procrustes.py
+++ procrustes.py
@@ -14,7 +14,7 @@ procrustes = Procrustes()
 
 class PBase(object):
     def __init__(self, data, validate=True):
-        self._data = data
+        self.raw_data = data
         self.validated_data = None
         self.error = None
         if validate:
@@ -25,7 +25,7 @@ class PBase(object):
         raise NotImplementedError, 'Define `configure` method'
 
     def validate(self):
-        if not self.required and self._data is None:
+        if not self.required and self.raw_data is None:
             return None
         return self.real_validate()
 
@@ -62,14 +62,14 @@ class PBase(object):
 class PTuple(PBase):
     @staticmethod
     def configure(cls, *types):
-        cls._types = types
+        cls.types = types
 
     def real_validate(self):
-        data = tuple(self._data)
-        if len(self._types) != len(data):
-            raise ValidationError('Must be %i elements iterable'
-                                  % len(self._types))
-        instances = [t(value, True) for t, value in zip(self._types, data)]
+        data = tuple(self.raw_data)
+        if len(self.types) != len(data):
+            raise ValidationError('Must be iterable of length %i'
+                                  % len(self.types))
+        instances = [t(value, True) for t, value in zip(self.types, data)]
         errors = [i.error for i in instances if i.error]
         if errors:
             raise ValidationError(errors)
@@ -100,18 +100,18 @@ class PTuple(PBase):
                 # TODO OMG
                 collector.extend([None] * (number - i))
                 i = number - 1
-            collector.append(cls._types[number].bulge(flatchild))
+            collector.append(cls.types[number].bulge(flatchild))
             i = i + 1
         return collector
 
 
 class PList(PBase):
     @staticmethod
-    def configure(cls, _type):
-        cls._type = _type
+    def configure(cls, type):
+        cls.type = type
 
     def real_validate(self):
-        instances = [self._type(i, True) for i in list(self._data)]
+        instances = [self.type(i, True) for i in list(self.raw_data)]
         errors = [i.error for i in instances if i.error]
         if errors:
             raise ValidationError(errors)
@@ -131,19 +131,19 @@ class PList(PBase):
 
     @classmethod
     def bulge(cls, flat, delimiter='__'):
-        return [cls._type.bulge(flatchild) for flatchild in
+        return [cls.type.bulge(flatchild) for flatchild in
                 group_by_key(flat, delimiter).itervalues()]
 
 
 class PDict(PBase):
     @staticmethod
     def configure(cls, named_types):
-        cls._named_types = named_types
+        cls.named_types = named_types
 
     def real_validate(self):
         instances = {}
-        for name, typ in self._named_types.iteritems():
-            instances[name] = typ(self._data.get(name, None), True)
+        for name, typ in self.named_types.iteritems():
+            instances[name] = typ(self.raw_data.get(name, None), True)
         errors = dict((name, value.error) for name, value
                       in instances.iteritems() if value.error)
         if errors:
@@ -167,7 +167,7 @@ class PDict(PBase):
     def bulge(cls, flat, delimiter='__'):
         result = {}
         grouped = group_by_key(flat, delimiter)
-        for name, ftype in cls._named_types.items():
+        for name, ftype in cls.named_types.items():
             flatchild = grouped.pop(name, None)
             result[name] = ftype.bulge(flatchild)
         # TODO we may have unmatched data in `grouped`
@@ -182,7 +182,7 @@ class PString(PBase):
         cls.regex = regex
 
     def real_validate(self):
-        s = str(self._data)
+        s = str(self.raw_data)
         slen = len(s)
 
         if self.min_length is not None and slen < self.min_length:
@@ -201,7 +201,7 @@ class PInteger(PBase):
 
     def real_validate(self):
         try:
-            i = int(self._data)
+            i = int(self.raw_data)
         except (ValueError, TypeError):
             raise ValidationError('Must be number, not a string')
 
