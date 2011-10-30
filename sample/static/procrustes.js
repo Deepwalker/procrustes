7890a5f5c513b4d21a7e260c65002d8d3830c1f3
Mihail Krivushin <krivushinme@gmail.com>
2011-10-30 22:46:01 +0400

    Fix js

--- sample/static/procrustes.js
+++ sample/static/procrustes.js
@@ -1,30 +1,33 @@
 function get_html(elem) {
-    return $('<div>').append(elem.clone()).remove().html()
+    return $('<div>').append(elem.clone()).remove().html();
 }
 
 $(function(){
+    "use strict";
+
     // Add new item for list
     window.widget_num = 10000;
     $(".procrustes__placeholder").each(function (i, d) {
-        var id = d.id.split('__')
-        id.splice(0, 1)
-        id = id.join('__')
-        var template_name = "#template__" + id
-        var template_widgets = get_html($(template_name))
-//        console.log(d, id)
+        var id = d.id.split('__'),
+            template_name = '',
+            template_widgets = '';
+
+        id.splice(0, 1);
+        id = id.join('__');
+
+        template_name = "#template__" + id;
+        template_widgets = get_html($(template_name));
+
         $("#add__" + id).click(function () {
-                var widgets = ('' + template_widgets).replace(/%s/g, window.widget_num)
-                window.widget_num += 1
-                console.log(window.widget_num)
-                console.log(widgets)
-                $(d).append(widgets)
-        })
-    })
+                var widgets = (template_widgets.toString()).replace(/%s/g, window.widget_num);
+                window.widget_num += 1;
+                $(d).append(widgets);
+        });
+    });
 
     // Delete list item
     $(".form_delete").live('click', function(){
-        el = this.parentNode
-        console.log(el)
-        el.parentNode.removeChild(el)
-    })
-})
+        var el = this.parentNode;
+        el.parentNode.removeChild(el);
+    });
+});
